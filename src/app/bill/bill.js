87453fb86feae6d2ccf55f22dd63abbae52efdfd
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2016-12-19 21:08:43 -0200

    Added Bill Component

---
+++ src/app/bill/bill.js
@@ -0,0 +1,37 @@
+/* global tinycolor */
+
+angular
+  .module('app')
+  .component('bill', {
+    templateUrl: 'app/bill/bill.html',
+    bindings: {
+      value: '@',
+      color: '@',
+      onAmountChanged: '&'
+    },
+    controller() {
+      const ctrl = this;
+      ctrl.sum = 0;
+
+      ctrl.$onChanges = () => {
+        const fontColor = tinycolor(ctrl.color);
+        fontColor.darken(50);
+        fontColor.saturate(10);
+
+        ctrl.bgcolor = {"background-color": ctrl.color, "color": fontColor.toHexString()};
+      };
+
+      ctrl.addAmount = amount => {
+        ctrl.amount = (ctrl.amount || 0) + amount;
+        ctrl.amountChanged();
+      };
+
+      ctrl.amountChanged = () => {
+        ctrl.sum = ctrl.amount * ctrl.value;
+
+        if (ctrl.onAmountChanged) {
+          ctrl.onAmountChanged({amount: ctrl.amount, sum: ctrl.sum});
+        }
+      };
+    }
+  });
--- src/app/counting/counting.js
+++ src/app/counting/counting.js
@@ -3,7 +3,15 @@ angular
   .component('counting', {
     templateUrl: 'app/counting/counting.html',
     controller() {
-      let vm = this;
+      const vm = this;
 
+      vm.bills = [
+        {value: 100, color: '#cdeaf0'},
+        {value: 50, color: '#f8e8c7'},
+        {value: 20, color: '#f9efcc'},
+        {value: 10, color: '#f2d0c7'},
+        {value: 5, color: '#f1ecf3'},
+        {value: 2, color: '#d9ecfa'}
+      ];
     }
   });
